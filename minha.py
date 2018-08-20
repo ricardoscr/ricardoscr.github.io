074140abc992ab92b52ad8eb2b7138d1d2cc8f70
Ricardo Carvalho <ricardosc@gmail.com>
2018-08-20 15:44:48 +0100

    Update minha.py

--- minha.py
+++ minha.py
@@ -1,5 +1,15 @@
 def bla():
-    print("DONE")
+    import urllib.request
+
+    url = 'http://bit.ly/carregarPy2'
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/7.0')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(url, './teste2.py')
+
+    ### Em nova celula....
+    import teste2 as t
+    t.foi()
 
 if __name__ == "__main__":
     print("Hello World!")
